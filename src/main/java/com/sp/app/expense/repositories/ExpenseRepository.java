517be6e5620ba3706d79dedc5deee164b6299a3b
itzshraddha <shraddha.dhope.25@gmail.com>
2022-04-10 13:45:41 +0530

    PRO-000|Shraddha|Initial commit for Expense-Ui

--- src/main/java/com/sp/app/expense/repositories/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sp.app.expense.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.sp.app.expense.entity.ExpenseEntity;
-
-public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Integer>{
-
-}
